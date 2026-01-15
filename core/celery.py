c68f85b631730d6681913237d2ae982a27f0a3fe
Aijamal <aijamal@MacBook-Air-Aijamal.local>
2026-01-15 17:31:38 +0600

    CELERY REDIS LOCAL

--- core/celery.py
+++ core/celery.py
@@ -20,21 +20,21 @@ app.autodiscover_tasks()
 # ========================
 
 app.conf.beat_schedule = {
-    # Пример: задача sync_data_from_cycle каждые 1 час
-    'sync-data-from-1c-every-hour': {
-        'task': 'apps.main.tasks.sync_data_from_cycle',  # путь к функции в tasks.py
+
+    'sync_data_from_db': {
+        'task': 'main.tasks.sync_data_from_db',  # путь к функции в tasks.py
         'schedule': crontab(minute='*/3'),  # каждый 3 минут
-        'options': {'queue': 'default'},  # очередь Celery (опционально)
+        # 'options': {'queue': 'default'},  # очередь Celery (опционально)
     },
 }
 
-# ========================
-# Общие настройки Celery
-# ========================
-app.conf.update(
-    timezone='Asia/Bishkek',     
-    enable_utc=True,
-    task_serializer='json',
-    accept_content=['json'],
-    result_serializer='json',
-)
+# # ========================
+# # Общие настройки Celery
+# # ========================
+# app.conf.update(
+#     timezone='Asia/Bishkek',     
+#     enable_utc=True,
+#     task_serializer='json',
+#     accept_content=['json'],
+#     result_serializer='json',
+# )
--- main/tasks.py
+++ main/tasks.py
@@ -3,13 +3,8 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-@shared_task(
-    bind=True,
-    name='apps.main.tasks.sync_data_from_db'
-)
+@shared_task(bind=True)
 def sync_data_from_db(self):
-    """
-    Простая задача: выводит числа от 1 до 10
-    """
     for i in range(1, 11):
         logger.info(f"[sync_data_from_db] Счёт: {i}")
+
